508f300f14e7fd91bf3775864bbe16b8ef3bbe31
Harun Can Yıldırım <eharuncan@gmail.com>
2022-10-06 16:54:19 +0300

    proje java console uygulamasına çevrildi.

--- api/expenses/expenses.publications.js
+++
@@ -1,6 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { ExpensesCollection } from './expenses.collection';
-
-Meteor.publish('expensesByLoggedUser', function publishExpenses() {
-  return ExpensesCollection.find({ userId: this.userId });
-});
