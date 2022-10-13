a6351583c3da715be6ac7e4e89f052e3d683e12a
Harun Can Yıldırım <eharuncan@gmail.com>
2022-10-13 16:01:39 +0300

    refactoring

--- src/main/java/org/example/app/services/ExpenseCategoryService.java
+++ src/main/java/org/example/app/services/ExpenseCategoryService.java
@@ -2,7 +2,6 @@ package org.example.app.services;
 
 import org.example.app.domain.ExpenseCategory;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
